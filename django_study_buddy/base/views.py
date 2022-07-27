b2c8d9c637dda15d344504412fdf6e1f3aab9330
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-07-27 14:58:53 +0100

    views page

--- django_study_buddy/base/views.py
+++ django_study_buddy/base/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse('home page')
+
+def room(request):
+    return HttpResponse('ROOM')
--- django_study_buddy/django_study_buddy/urls.py
+++ django_study_buddy/django_study_buddy/urls.py
@@ -1,13 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
 
 
-def home(request):
-    return HttpResponse('home page')
-
-def room(request):
-    return HttpResponse('ROOM')
+    
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
