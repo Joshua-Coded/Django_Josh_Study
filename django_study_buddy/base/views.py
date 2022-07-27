b8f9b0401e9c13b8542576adf48beee536f0fd0d
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-07-27 16:04:14 +0100

    templating

--- django_study_buddy/base/views.py
+++ django_study_buddy/base/views.py
@@ -3,8 +3,15 @@ from django.shortcuts import render
 
 # Create your views here.
 
+rooms = [
+    {'id':1, 'name':'Lets learn python!'},
+    {'id':2, 'name':'Lets learn C programming!'},
+    {'id':3, 'name':'Lets  learn Vue.js!'},
+]
+
+
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'rooms':rooms})
 
 def room(request):
     return render(request, 'room.html')
