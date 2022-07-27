896c4b04d7c5e9df9a02a62254f607eded753c13
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-07-27 14:57:02 +0100

    room setup

--- django_study_buddy/django_study_buddy/urls.py
+++ django_study_buddy/django_study_buddy/urls.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
 
+
 def home(request):
     return HttpResponse('home page')
 
-
+def room(request):
+    return HttpResponse('ROOM')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
+    path('room/', room)
 ]
