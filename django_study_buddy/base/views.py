9c3f779fe89f641ceb70948c5c7bf84c389146f9
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-07-27 16:40:58 +0100

    done for the day, continue tomorrow

--- django_study_buddy/base/views.py
+++ django_study_buddy/base/views.py
@@ -14,5 +14,11 @@ def home(request):
     context = {'rooms':rooms}
     return render(request, 'base/home.html', context)
 
+
 def room(request, pk):
-    return render(request, 'base/room.html')
+    room = None
+    for i in rooms:
+    if i['id'] == int(pk):
+        room = i
+        context = {'room':room}
+        return render(request, 'base/room.html', context)
