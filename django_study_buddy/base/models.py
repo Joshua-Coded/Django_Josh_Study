c2a5ece592be9acab7ed7a6b6ce1155aa1e671e6
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-07-28 16:11:23 +0100

    added the topic, message, models

--- django_study_buddy/base/models.py
+++ django_study_buddy/base/models.py
@@ -1,10 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
+
+class Topic(models.Model):
+    name= models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+#Rooms model
 class Room(models.Model):
-    #host =
-    #topic = 
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True) 
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     #participants =
@@ -14,3 +25,31 @@ class Room(models.Model):
 
     def __str__(self):
         return self.name
+
+#Message model
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    
+
+    def __str__(self):
+        return self.body[0:50]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
