8370395eb262a6bcdf502d03a6da4bd6cac7dcb1
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-07-28 15:53:01 +0100

    model

--- django_study_buddy/base/views.py
+++ django_study_buddy/base/views.py
@@ -4,11 +4,11 @@ from .models import Room
 
 # Create your views here.
 
-rooms = [
-    {'id':1, 'name':'Lets learn python!'},
-    {'id':2, 'name':'Lets learn C programming!'},
-    {'id':3, 'name':'Lets  learn Vue.js!'},
-]
+# rooms = [
+#     {'id':1, 'name':'Lets learn python!'},
+#     {'id':2, 'name':'Lets learn C programming!'},
+#     {'id':3, 'name':'Lets  learn Vue.js!'},
+# ]
 
 
 def home(request):
@@ -18,9 +18,8 @@ def home(request):
 
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-        context = {'room':room}
-        return render(request, 'base/room.html', context)
+    room = Room.objects.get(id=pk)
+
+
+    context = {'room':room}
+    return render(request, 'base/room.html', context)
