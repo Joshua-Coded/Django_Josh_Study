fae54e1a033e415ad37e9616c360769a3d742b8b
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-07-28 15:03:35 +0100

    indentation error fixed

--- django_study_buddy/base/views.py
+++ django_study_buddy/base/views.py
@@ -18,7 +18,7 @@ def home(request):
 def room(request, pk):
     room = None
     for i in rooms:
-    if i['id'] == int(pk):
-        room = i
+        if i['id'] == int(pk):
+            room = i
         context = {'room':room}
         return render(request, 'base/room.html', context)
