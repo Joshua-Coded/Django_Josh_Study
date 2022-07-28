23609f46c3de9c49a7d120ce440e183596b56d5e
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-07-28 15:44:17 +0100

    room model

--- django_study_buddy/base/views.py
+++ django_study_buddy/base/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
+from .models import Room
 
 
 # Create your views here.
 
-rooms = [
-    {'id':1, 'name':'Lets learn python!'},
-    {'id':2, 'name':'Lets learn C programming!'},
-    {'id':3, 'name':'Lets  learn Vue.js!'},
-]
+# rooms = [
+#     {'id':1, 'name':'Lets learn python!'},
+#     {'id':2, 'name':'Lets learn C programming!'},
+#     {'id':3, 'name':'Lets  learn Vue.js!'},
+# ]
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms':rooms}
     return render(request, 'base/home.html', context)
 
