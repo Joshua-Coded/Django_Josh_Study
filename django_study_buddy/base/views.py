4640e4c0753e685c93d9b124a77ad821afb4ba45
Ayanfe <oluwafemi.wafi@gmail.com>
2022-07-30 14:53:55 +0100

    redirection

--- django_study_buddy/base/views.py
+++ django_study_buddy/base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
 from .forms import RoomForm
 
@@ -27,7 +27,11 @@ def room(request, pk):
 def createRoom(request):
     form = RoomForm()
     if request.method == "POST":
-        print(request.POST)
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+                    
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
 
