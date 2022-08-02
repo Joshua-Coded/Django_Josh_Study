747f2839ba30f4f2c2bec2308605cef204bf9e6f
Ayanfe <oluwafemi.wafi@gmail.com>
2022-08-02 13:29:14 +0100

    updated the urls file

--- django_study_buddy/base/urls.py
+++ django_study_buddy/base/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
 
     path('create-room/', views.createRoom, name="create-room"),
     path('update-room<str:pk>/', views.updateRoom, name="update-room"),
+    path('delete-room<str:pk>/', views.deleteRoom, name="delete-room"),
 ]
