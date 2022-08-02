8bb7c9c5d0867bb903a705ad1ab7771adf796192
Ayanfe <oluwafemi.wafi@gmail.com>
2022-08-02 13:27:35 +0100

    added the delete functionality

--- django_study_buddy/base/views.py
+++ django_study_buddy/base/views.py
@@ -48,6 +48,12 @@ def updateRoom(request, pk):
     context = {'form': form}
     return render(request, 'base/room_form.html', context)    
 
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': room})
 
 
 
