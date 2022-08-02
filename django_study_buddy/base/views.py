4043348c3b4c3403032dce4707f852b1eff35d1f
Ayanfe <oluwafemi.wafi@gmail.com>
2022-08-02 13:40:45 +0100

    context dictionary updated

--- django_study_buddy/base/views.py
+++ django_study_buddy/base/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 
 # Create your views here.
@@ -13,7 +13,10 @@ from .forms import RoomForm
 
 def home(request):
     rooms = Room.objects.all()
-    context = {'rooms':rooms}
+
+    topics = Topic.objects.all()
+
+    context = {'rooms':rooms, 'topics': topics}
     return render(request, 'base/home.html', context)
 
 
