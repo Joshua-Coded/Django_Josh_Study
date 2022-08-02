fa353100aa9488b2a88fa97627b069674aeffe15
Ayanfe <oluwafemi.wafi@gmail.com>
2022-08-02 13:51:39 +0100

    search query

--- django_study_buddy/base/views.py
+++ django_study_buddy/base/views.py
@@ -12,7 +12,9 @@ from .forms import RoomForm
 
 
 def home(request):
-    rooms = Room.objects.all()
+    q= request.GET.get('q') if request.GET.get('q') != None else ''
+
+    rooms = Room.objects.filter(topic__name__icontains=q)
 
     topics = Topic.objects.all()
 
