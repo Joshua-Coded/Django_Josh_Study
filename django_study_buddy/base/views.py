3e4a37794bde76be3e211c8715eaf5af63b7535c
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-08-04 11:53:31 +0100

    all

--- django_study_buddy/base/views.py
+++ django_study_buddy/base/views.py
@@ -1,3 +1,5 @@
+from unicodedata import name
+from django.db.models import Q
 from django.shortcuts import render, redirect
 from .models import Room, Topic
 from .forms import RoomForm
@@ -14,7 +16,11 @@ from .forms import RoomForm
 def home(request):
     q= request.GET.get('q') if request.GET.get('q') != None else ''
 
-    rooms = Room.objects.filter(topic__name__icontains=q)
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
 
     topics = Topic.objects.all()
 
