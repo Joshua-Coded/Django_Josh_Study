635be594e9127d8bec3194e9757cdc070347493c
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-08-04 12:02:11 +0100

    rooms

--- django_study_buddy/base/views.py
+++ django_study_buddy/base/views.py
@@ -23,8 +23,9 @@ def home(request):
     )
 
     topics = Topic.objects.all()
+    room_count = rooms.count()
 
-    context = {'rooms':rooms, 'topics': topics}
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
     return render(request, 'base/home.html', context)
 
 
